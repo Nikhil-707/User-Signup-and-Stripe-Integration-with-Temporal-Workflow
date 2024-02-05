801fbc8ca84ef642da833acb637e5979894b7f42
Nikhil Kumar Nayak <139217655+Nikhil-707@users.noreply.github.com>
2024-02-06 02:32:32 +0530

    Add files via upload

--- src/main/test/java/com/midas/app/ApplicationTest.java
+++ src/main/test/java/com/midas/app/ApplicationTest.java
@@ -1,1 +1,21 @@
+package com.midas.app;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+class ApplicationTest {
+
+	@Autowired
+	private ApplicationContext applicationContext;
+
+	@Test
+	void contextLoads() {
+		// Check if the Spring application context is not null
+		assertNotNull(applicationContext);
+	}
+}
